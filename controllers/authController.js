6c33544c721d2fcb2cc6936364e418c0f1a64597
AbhijeetGarg2004 <ajgarg04@gmail.com>
2025-03-15 23:40:54 +0530

    work in prog

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+import express, { Router } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+//registering an user
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
