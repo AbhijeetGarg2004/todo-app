0fb057131192b782f13ea581763b64a1c6aa074f
AbhijeetGarg2004 <ajgarg04@gmail.com>
2025-03-16 23:49:30 +0530

    Workinn

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON
+app.use("/api/auth", authRoutes);// Use auth routes
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
